a0ebb882100c7a431463ebb11f1cfa536dd0a494
Tucker Taft <taft@adacore.com>
2024-11-21 14:56:52 +0000

    Split the Test_Bad_Units func out of units.psl since now a compile-time err
    Now that we are detecting things like unit mismatches at compile-time,
    we need to put the check for this in a separate test.

--- testsuite/ParaSail/units/test.sh
+++ testsuite/ParaSail/units/test.sh
@@ -1,3 +1,9 @@
 RUN="psli ../aaa.psi"
 
+echo "--- units.psl ---"
+echo ""
 $RUN units.psl -command Test_Units
+echo ""
+echo "--- bad_units.psl ---"
+echo ""
+$RUN units.psl bad_units.psl -command Test_Units
